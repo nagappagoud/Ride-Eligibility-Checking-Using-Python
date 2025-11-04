3e13600cbc9ea0cd35f15adaca46141234ed386a
Nagappagoud Patil <nagappagouda1925@gmail.com>
2025-11-04 20:16:09 +0530

    Add files via upload

---
+++ Ride Eligibility Checking Using Python.py
@@ -0,0 +1,14 @@
+print("check the Ride Eligibility")
+hight=int(input("enter your hight"))
+age=int(input("enter your age"))
+if hight>120:
+    print("you are Eligibil for Ride")
+else:
+    print("you are not Eligibil for Ride")
+
+if age>18:
+    print("ticket price is 12$")
+else:
+    print("ticket price is 7$")
+
+ 
